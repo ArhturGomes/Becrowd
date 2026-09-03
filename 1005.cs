2ec2880ef28a8af09ed72534c918b5c7f24d3b8f
ArhturGomes <arthurg1810@gmail.com>
2026-09-03 17:19:29 -0300

    1005.CS

---
+++ 1005.cs
@@ -0,0 +1,14 @@
+using System;
+
+class URI
+{
+    static void Main(string[] args)
+    {
+        double A = double.Parse(Console.ReadLine());
+        double B = double.Parse(Console.ReadLine());
+
+        double MEDIA = (A * 3.5 + B * 7.5) / 11.0;
+
+        Console.WriteLine("MEDIA = {0:F5}", MEDIA);
+    }
+}
